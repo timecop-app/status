3669eae7ae8e8222dff00e6349c74d8d1c9ba6ca
Guillaume Briday <guillaumebriday@gmail.com>
2019-10-13 21:11:55 +0200

    Updating statusfy config

--- config.js
+++ config.js
@@ -1,17 +1,33 @@
 module.exports = {
-  "title": "timecop-status",
-  "name": "timecop_status",
-  "description": "The status page for Timecop",
-  "defaultLocale": "en",
-  "locales": [
+  title: "timecop-status",
+  name: "timecop_status",
+  description: "The status page for Timecop",
+  serviceWorker: false,
+  manifest: false,
+  analytics: {
+    ga: 'UA-54003772-13',
+  },
+  defaultLocale: "en",
+  locales: [
     {
-      "code": "en",
-      "iso": "en-US",
-      "name": "English",
-      "file": "en.json"
+      code: "en",
+      iso: "en-US",
+      name: "English",
+      file: "en.json"
     }
   ],
-  "content": {
-    "frontMatterFormat": "yaml"
+  content: {
+    frontMatterFormat: "yaml",
+    systems: [
+      'api',
+      'site-delivery'
+    ]
+  },
+  theme: {
+    links: {
+      en: {
+        home: "https://timecop-app.com/"
+      }
+    }
   }
 }
