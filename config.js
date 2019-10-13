bcb39dda3fe07b885d5f70c6b48c7313f6c500cd
Guillaume Briday <guillaumebriday@gmail.com>
2019-10-13 20:37:27 +0200

    Configuring statusfy

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "timecop-status",
+  "name": "timecop_status",
+  "description": "The status page for Timecop",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml"
+  }
+}
